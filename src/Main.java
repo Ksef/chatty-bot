ac4870d5a930f03d69ca471ddfe3f78f7d775ff5
Roman Dyriv <xephosroman@gmail.com>
2021-12-06 13:23:38 +0200

    change access modifiers; move run() to the top; create object on main().

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 public class Main {
 
-    static SimpleBot simpleBot = new SimpleBot();
-
     public static void main(String[] args) {
+        SimpleBot simpleBot = new SimpleBot();
         simpleBot.run();
     }
 }
